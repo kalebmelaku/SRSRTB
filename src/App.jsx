3202a99521656b20e60fa260a45a16a680aee451
kalebmelaku <k.melaku@cellutech.net>
2023-05-06 09:24:19 +0300

    OWDA-PMS

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import Banner from "./components/banner"
-// import NavBar from "./components/navbar"
-// import About from "./components/about"
-// import Vacancy from "./components/vacancy"
-// import Partners from "./components/partners"
-// import Carousal from "./components/carousal"
-// export default function App() {
-
-
-//   return (
-//     <>
-//       <Banner/>     
-//       <NavBar/>
-//       <Carousal/>
-//       <About/>
-//       <Partners/>
-//       <Vacancy/>
-//       <Banner/>     
-//     </>
-//   )
-// }
-
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/home'
 import Jobs from './pages/job'
